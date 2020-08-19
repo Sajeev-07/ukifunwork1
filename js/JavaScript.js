36d54fb650dc26e4627d01a16830e56b1c83e3b6
sajeev-07 <sajeevpriyakannan@gmail.com>
2020-08-19 11:51:26 +0530

    updated

---
+++ js/JavaScript.js
@@ -0,0 +1,174 @@
+﻿
+var id_name;
+var valid_name;
+var valid_pass;
+var valid_mail;
+
+function login_validation() {
+
+
+    var pwd_id = document.getElementById("pwd");
+
+    if (pwd_id.value != "") {
+    } else {
+        document.getElementById("validation_p").innerHTML = "this field can't be empty";
+    }
+}
+
+function login() {
+    var user_id = ["Sajeev", "Ukistuent", "Ukiadmin", "Nayan", "Rajini"]
+    var user_name = ["sajeev@gmail.com", "ukistudent@gmail.com", "ukiadmin@gmail.com", "nayan@gmail.com", "rajini@gmail.com"];
+    var password = ["Sajeev@123!", "Ukistu@123!", "Ukiadmin@123!", "Nayan@123!", "Rajini@123!"];
+
+
+    var mail_id = document.getElementById("email");
+    var pwd_id = document.getElementById("pwd");
+
+    var mail_id = document.getElementById("email");
+
+    var mail_validation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var valid_check;
+
+
+    if (mail_id.value != "") {
+
+        if (mail_id.value.match(mail_validation)) {
+            document.getElementById("validation_e").innerHTML = "";
+            for (i = 0; i < user_name.length; i++) {
+
+                var name = user_id[i];
+                var x = user_name[i];
+                var y = password[i];
+
+
+                if ((x == mail_id.value) && (y == pwd_id.value)) {
+                    valid_check = "true";
+                }
+            }
+
+            if (valid_check == "true") {
+                alert("login successful" + id_name);
+            } else {
+                alert("Wrong user name and password");
+                location.reload();
+            }
+
+        } else
+            document.getElementById("validation_e").innerHTML = "Enter a valid email id";
+
+    } else {
+        document.getElementById("validation_e").innerHTML = "this field can't be empty";
+    }
+
+}
+
+/*----------Sign Up---------------*/
+
+function name_validation() {
+
+    var user_id = document.getElementById("user1");
+    var letters = /^[A-Za-z]+$/;
+
+    if (user_id.value != "") {
+        if (user_id.value.match(letters)) {
+
+            if (user_id.value.length < 5) {
+                document.getElementById("validation_n").innerHTML = "Name should be contain atleast 5 letters";
+            } else {
+                document.getElementById("validation_n").innerHTML = "";
+                valid_name = "true";
+            }
+
+        } else {
+            document.getElementById("validation_n").innerHTML = "Type alphabet letters only";
+
+        }
+    } else {
+        document.getElementById("validation_n").innerHTML = "this field can't be empty";
+    }
+}
+
+
+function myFunction() {
+    var mail_id = document.getElementById("email");
+
+    var mail_validation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (mail_id.value != "") {
+
+        if (mail_id.value.match(mail_validation)) {
+            document.getElementById("validation_e").innerHTML = "";
+            valid_mail = "true";
+        } else
+            document.getElementById("validation_e").innerHTML = "Enter a valid email id";
+
+    } else {
+        document.getElementById("validation_e").innerHTML = "this field can't be empty";
+    }
+
+
+}
+
+function password_validation() {
+
+
+    var pwd_id = document.getElementById("pwd");
+    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+    var mediumRegex = new RegExp("^(?=.{7,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+    var enoughRegex = new RegExp("(?=.{6,}).*", "g");
+
+    if (pwd_id.value != "") {
+
+        if (pwd_id.value.length <= 8) {
+
+            document.getElementById("validation_p").innerHTML = "include Upper and lower case letters, numbers and Charecters";
+            document.getElementById("password_color_valid").className = "mystyle";
+        }
+        else if (false == enoughRegex.test(pwd_id.value)) {
+            document.getElementById("validation_p").innerHTML = "include Upper and lower case letters, numbers and Charecters";
+            document.getElementById("password_color_valid").className = "mystyle";
+
+
+
+        } else if (strongRegex.test(pwd_id.value)) {
+            document.getElementById("password_color_valid").className = "mystyle2";
+            valid_pass = "true";
+            document.getElementById("validation_p").innerHTML = "";
+        } else if (mediumRegex.test(pwd_id.value)) {
+            document.getElementById("password_color_valid").className = "mystyle1";
+            document.getElementById("validation_p").innerHTML = "";
+            valid_pass = "true";
+        } else {
+            document.getElementById("password_color_valid").className = "mystyle";
+        } 
+
+    } else {
+        document.getElementById("validation_p").innerHTML = "this field can't be empty";
+    }
+}
+
+function signupFunc() {
+
+    if (valid_mail == "true" && valid_name == "true" && valid_pass == "true") {
+        alert("successful");
+    }
+    else if (valid_mail == "true") {
+        document.getElementById("validation_e").innerHTML = "";
+        document.getElementById("validation_n").innerHTML = "Please enter valid data";
+        document.getElementById("validation_p").innerHTML = "Please enter valid data";
+    } else if (valid_name == "true") {
+        document.getElementById("validation_n").innerHTML = "";
+        document.getElementById("validation_e").innerHTML = "Please enter valid data";
+        document.getElementById("validation_p").innerHTML = "Please enter valid data";
+    } else if (valid_pass == "true") {
+        document.getElementById("validation_p").innerHTML = "";
+        document.getElementById("validation_n").innerHTML = "Please enter valid data";
+        document.getElementById("validation_e").innerHTML = "Please enter valid data";
+    } else {
+
+        document.getElementById("validation_p").innerHTML = "Please enter valid data";
+        document.getElementById("validation_n").innerHTML = "Please enter valid data";
+        document.getElementById("validation_e").innerHTML = "Please enter valid data";
+    }
+
+}
---
+++ js/player.js
@@ -0,0 +1,451 @@
+
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+
+
+$(document).ready(function() { 
+  SC.stream('/tracks/653922530',function(sound){
+
+      //play button
+      $('#start3').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop3').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev3').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next3').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/862953466',function(sound){
+
+      //play button
+      $('#start4').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop4').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev4').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next4').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/724384264',function(sound){
+
+      //play button
+      $('#start5').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop5').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev5').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next5').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/45791711',function(sound){
+
+      //play button
+      $('#start6').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop6').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev6').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next6').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/740748694',function(sound){
+
+      //play button
+      $('#start7').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop7').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev7').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next7').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/665105183',function(sound){
+
+      //play button
+      $('#start8').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop8').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev8').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next8').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/845201014',function(sound){
+
+      //play button
+      $('#start9').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop9').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev9').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next9').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/227982318',function(sound){
+
+      //play button
+      $('#start10').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop10').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev10').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next10').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/693696028',function(sound){
+
+      //play button
+      $('#start11').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop11').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev11').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next11').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/693696028',function(sound){
+
+      //play button
+      $('#start12').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop12').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev12').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next12').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/693696028',function(sound){
+
+      //play button
+      $('#start').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
+
+ $(document).ready(function() { 
+  SC.stream('/tracks/693696028',function(sound){
+
+      //play button
+      $('#start').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+
+      //stop button
+      $('#stop').click(function(e) {
+          e.preventDefault();
+          sound.stop();
+        });
+
+        //previous button
+        $('#prev').click(function(e) {
+          e.preventDefault();
+          sound.previous();
+        });
+
+
+        //next button
+        $('#next').click(function(e) {
+          e.preventDefault();
+          sound.next();
+        });
+
+
+
+
+
+   });
+ });
+
